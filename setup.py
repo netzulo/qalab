b3b93a2ec2522cfce27753cd4382e20cf2573fb0
crypto netzulo <crypto.netzulo@gmail.com>
2017-09-10 16:20:20 +0200

    [qalab] UPDATE to v0.0.6

--- setup.py
+++ setup.py
@@ -7,14 +7,20 @@ from os import name
 CURR_PATH = path.abspath(path.dirname(__file__))
 sys.path.append(path.join(CURR_PATH, 'qalab'))
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(name='qalaboratory',
-      version='0.0.5',
+      version='0.0.6',
       packages=find_packages(exclude=['tests']),
       description = 'QALAB, proyect manager for QA open source proyects',
+
       author = 'Netzulo Open Source',
       author_email = 'netzuleando@gmail.com',
       url = 'https://github.com/netzulo/qalab',
-      download_url = 'https://github.com/netzulo/qalab/tarball/v0.0.5',
+      download_url = 'https://github.com/netzulo/qalab/tarball/v0.0.6',
       keywords = ['testing', 'logging', 'functional','selenium', 'test', 'installer', 'hub', 'node'],
       install_requires=['appdirs','packaging==16.8','pyparsing','six==1.10.0',
       'nose==1.3.7','nose-testconfig==0.10', 'wget'],
