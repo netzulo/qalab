f313b6814e15af84d4cb53f17ded45106b6ce91f
crypto netzulo <crypto.netzulo@gmail.com>
2017-09-10 17:44:15 +0200

    [qalab] Added long description to setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='qalaboratory',
       version='0.0.6',
       packages=find_packages(exclude=['tests']),
       description = 'QALAB, proyect manager for QA open source proyects',
-
+      long_description=readme(),
       author = 'Netzulo Open Source',
       author_email = 'netzuleando@gmail.com',
       url = 'https://github.com/netzulo/qalab',
