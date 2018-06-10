4fbb805213c1cfd0e85b27ebecd995a0f1d6fa7d
netzulo <netzuleando@gmail.com>
2018-06-10 21:27:15 +0200

    [qalab] Update to v0.1.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 from setuptools import setup, find_packages
 
 
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 CURR_PATH = path.abspath(path.dirname(__file__))
 # make works script on windows
 sys.path.append(path.join(CURR_PATH, 'qalab'))
